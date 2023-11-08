94561bc6c924fe4394bb73760348a8c412649792
barbiepavez <149853412+barbiepavez@users.noreply.github.com>
2023-11-07 23:52:19 -0300

    Update calcuradora.py
    Agregue la opcion de multiplicar al menu

--- calcuradora.py
+++ calcuradora.py
@@ -14,6 +14,7 @@ def menu():
     print("Select operation:")
     print("1. Suma")
     print("2. Resta")
+    Print("3, multiplicacion")
 
     choice = int(input("Enter choice: "))
     return choice
@@ -27,6 +28,11 @@ def perform_operation(choice):
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", subtract(a, b))
+        
+    elif choice == 3:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+        print("Result: ", multiply(a, b))
 def main():
     while True:
         choice = menu()
