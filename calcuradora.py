2335ebc77031bf9207b80dad5aec419136d034df
barbiepavez <149853412+barbiepavez@users.noreply.github.com>
2023-11-08 00:06:19 -0300

    Update calcuradora.py

--- calcuradora.py
+++ calcuradora.py
@@ -14,7 +14,7 @@ def menu():
     print("Select operation:")
     print("1. Suma")
     print("2. Resta")
-    print("3, multiplicacion")
+   
 
     choice = int(input("Enter choice: "))
     return choice
@@ -30,10 +30,7 @@ def perform_operation(choice):
         b = float(input("Enter second number: "))
         print("Result: ", subtract(a, b))
         
-    elif choice == 3:
-        a = float(input("Enter first number: "))
-        b = float(input("Enter second number: "))
-        print("Result: ", multiply(a, b))
+
         
 def main():
     while True:
